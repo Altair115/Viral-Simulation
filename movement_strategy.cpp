2a3ebb32e76e9dab6da7c52de0b493603a2c8d8a
Kevin <kevin.celinski@outlook.com>
2020-04-28 14:52:16 +0200

    :sparkles: for Strats

---
+++ movement_strategy.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Kevin on 28/04/2020.
+//
+
+#include "movement_strategy.h"
+
+void RegularMovementStrategy::set_x(double x){this->_x = x;};
+void RegularMovementStrategy::set_y(double y){this->_y = y;};
+void RegularMovementStrategy::set_dx(double x){this->_dx = x;};
+void RegularMovementStrategy::set_dy(double y){this->_dy = y;};
+
+void LockdownMovementStrategy::set_x(double x){};
+void LockdownMovementStrategy::set_y(double y){};
+void LockdownMovementStrategy::set_dx(double x){};
+void LockdownMovementStrategy::set_dy(double y){};
